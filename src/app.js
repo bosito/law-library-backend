b258adb86a4fdf72dd721dd1028023d7e913d67f
DESKTOP-KHKBE2E\bosito <bosito321@gmail.com>
2022-04-16 14:22:57 -0500

    .

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 import express from 'express';
-import morgan from 'morgan';
-
-import userRoutes from './routes/users.routes.js'
+//import morgan from 'morgan';
+import Models from './models/init-models.js'; //esto es de prueba
+//import userRoutes from './routes/users.routes.js'
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
+//app.use(morgan('dev'));
+//app.use(express.json());
 
 //rutas
-app.use(userRoutes);
+//app.use(userRoutes);
+
+
+//abajo agrega los controladores
+
+const routes = express.Router();
+
+const { users, status, use_vip } = Models()
+
+routes.get('/test', async (request, response) => {
+    try {
+        const result = await use_vip.findAll();
+        response.json(result);
+
+    } catch (error) {
+        console.log('error -->', error)
+        response.status(400).json({
+            message: error.message
+        })
+    }
+});
+
+app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Todos API!',
+}));
 
 export default app;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,19 @@
 import { Router } from 'express';
+import Models from '../models/init-models.js';
 //abajo agrega los controladores
 
-const routes = Router()
+const routes = Router();
 
-routes.get('/users',()=>{});
+const { users } = Models()
+
+routes.get('/', async (request, response) => {
+    try {
+        const result = await users.findAll();
+        response.json(result);
+
+    } catch (error) {
+        response.status(400).json({
+            message: error.message
+        })
+    }
+});
